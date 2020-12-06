9736b95d2eb4d018e00e7d14d943c52fa13673ae
soo5717 <koo5717@naver.com>
2020-12-06 21:02:56 +0900

    add rm

--- modules/responseMessage.js
+++ modules/responseMessage.js
@@ -19,5 +19,7 @@ module.exports = {
 
     //프로필
     PROFILE_READ_SUCCESS: '프로필 조회 성공',
-    PROFILE_READ_FAIL: '프로필 조회 실패'
+    PROFILE_READ_FAIL: '프로필 조회 실패',
+    PROFIME_UPDATE_SUCCESS: '프로필 수정 성공',
+    PROFIME_UPDATE_FAIL: '프로필 수정 실패'
 };
