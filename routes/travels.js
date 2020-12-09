b9acbe9581101308606881f4cd88d29c4bf069ae
Jiseondf <48876106+Jiseondf@users.noreply.github.com>
2020-12-09 14:47:25 +0900

    travel get

--- routes/travels.js
+++ routes/travels.js
@@ -6,7 +6,10 @@ const travelController = require('../controllers/travelController');
 // [POST] 여행 생성
 router.post('/', checkToken, travelController.createTravel);
 
-// [GET] 여행 삭제
+// [DELETE] 여행 삭제
 router.delete('/', checkToken, travelController.deleteTravel);
+
+// [GET] 여행 목록 불러오기
+router.get('/', checkToken, travelController.getTravel);
 
 module.exports = router;
