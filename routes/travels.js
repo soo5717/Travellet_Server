7ae163fd32fff511467a929a99ae82db8eeccd4b
Jiseondf <48876106+Jiseondf@users.noreply.github.com>
2020-12-09 14:20:44 +0900

    travel del

--- routes/travels.js
+++ routes/travels.js
@@ -6,4 +6,7 @@ const travelController = require('../controllers/travelController');
 // [POST] 여행 생성
 router.post('/', checkToken, travelController.createTravel);
 
+// [GET] 여행 삭제
+router.delete('/', checkToken, travelController.deleteTravel);
+
 module.exports = router;
