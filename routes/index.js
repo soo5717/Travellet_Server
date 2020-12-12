3e2b0ce55400e7851f30d82a496d2b745b5d078d
Jiseondf <48876106+Jiseondf@users.noreply.github.com>
2020-12-12 17:46:30 +0900

    place url

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,6 @@ router.get('/', function(req, res) {
 // Router 추가
 router.use('/users', require('./users'));
 router.use('/travels', require('./travels'));
-router.use('/place/likes', require('./placeLikes'));//
+router.use('/likes', require('./placeLikes'));//
 
 module.exports = router;
