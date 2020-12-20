13bfdba04363192476126dfc4ac7ace824b8df88
soo5717 <koo5717@naver.com>
2020-12-20 10:05:14 +0900

    responseMessage

--- modules/responseMessage.js
+++ modules/responseMessage.js
@@ -7,17 +7,17 @@ module.exports = {
     EXPIRED_TOKEN: '토큰 값이 만료되었습니다.',
     INVALID_TOKEN: '유효하지 않은 토큰 값입니다.',
 
-    // 회원가입
+    // 회원가입(C)
     SIGNUP_SUCCESS: '회원가입 성공',
     SIGNUP_FAIL: '회원가입 실패',
 
-    //로그인
+    //로그인(R)
     SIGNIN_SUCCESS: '로그인 성공',
     NO_ACCOUNT: '존재하지 않는 계정입니다.',
     MISMATCH_PWD: '비밀번호가 일치하지 않습니다.',
     SIGNIN_FAIL: '로그인 실패',
 
-    //프로필
+    //프로필(RUD)
     PROFILE_READ_SUCCESS: '프로필 조회 성공',
     PROFILE_READ_FAIL: '프로필 조회 실패',
     PROFILE_UPDATE_SUCCESS: '프로필 수정 성공',
@@ -25,31 +25,49 @@ module.exports = {
     PROFILE_DELETE_SUCCESS: '회원 탈퇴 성공',
     PROFILE_DELETE_FAIL: '회원 탈퇴 실패',
 
-    //여행 생성
-    TRAVEL_CREATE_SUCCESS: '여행 생성 성공',
-    TRAVEL_CREATE_FAIL: '여행 생성 실패',
-
-    //여행 삭제
+    //여행(CRD)
+    TRAVEL_CREATE_SUCCESS: '여행 추가 성공',
+    TRAVEL_CREATE_FAIL: '여행 추가 실패',
+    TRAVEL_READ_SUCCESS: '여행 목록 조회 성공',
+    TRAVEL_READ_FAIL: '여행 목록 조회 실패',
     TRAVEL_DELETE_SUCCESS: '여행 삭제 성공',
     TRAVEL_DELETE_FAIL: '여행 삭제 실패',
 
-    //여행 목룍 불러오기
-    TRAVEL_READ_SUCCESS: '여행 목록 불러오기 성공',
-    TRAVEL_READ_FAIL: '여행 목록 불러오기 실패',
+    //일정(CRUD)
+    PLAN_CREATE_SUCCESS: '일정 추가 성공',
+    PLAN_CREATE_FAIL: '일정 추가 실패',
+    PLAN_READ_SUCCESS: '일정 목록 조회 성공',
+    PLAN_READ_FAIL: '일정 목록 조회 실패',
+    PLAN_UPDATE_SUCCESS: '일정 수정 성공',
+    PLAN_UPDATE_FAIL: '일정 수정 실패',
+    PLAN_DELETE_SUCCESS: '일정 삭제 성공',
+    PLAN_DELETE_FAIL: '일정 삭제 실패',
 
-    //장소 좋아요 추가
+    //예산(CRUD)
+    BUDGET_CREATE_SUCCESS: '예산 추가 성공',
+    BUDGET_CREATE_FAIL: '예산 추가 성공',
+    BUDGET_READ_SUCCESS: '예산 목록 조회 성공',
+    BUDGET_READ_FAIL: '예산 목록 조회 성공',
+    BUDGET_UPDATE_SUCCESS: '예산 수정 성공',
+    BUDGET_UPDATE_FAIL: '예산 수정 성공',
+    BUDGET_DELETE_SUCCESS: '예산 삭제 성공',
+    BUDGET_DELETE_FAIL: '예산 삭제 성공',
+
+    //지출(CRUD)
+    EXPENSE_CREATE_SUCCESS: '지출 추가 성공',
+    EXPENSE_CREATE_FAIL: '지출 추가 성공',
+    EXPENSE_READ_SUCCESS: '지출 목록 조회 성공',
+    EXPENSE_READ_FAIL: '지출 목록 조회 성공',
+    EXPENSE_UPDATE_SUCCESS: '지출 수정 성공',
+    EXPENSE_UPDATE_FAIL: '지출 수정 성공',
+    EXPENSE_DELETE_SUCCESS: '지출 삭제 성공',
+    EXPENSE_DELETE_FAIL: '지출 삭제 성공',
+
+    //장소 좋아요(CRD)
     PLACE_LIKE_CREATE_SUCCESS: '장소 좋아요 추가 성공',
     PLACE_LIKE_CREATE_FAIL: '장소 좋아요 추가 실패',
-
-    //장소 좋아요 삭제
+    PLACE_LIKE_READ_SUCCESS: '장소 좋아요 목록 조회 성공',
+    PLACE_LIKE_READ_FAIL: '장소 좋아요 목록 조회 실패',
     PLACE_LIKE_DELETE_SUCCESS: '장소 좋아요 삭제 성공',
-    PLACE_LIKE_DELETE_FAIL: '장소 좋아요 삭제 실패',
-
-    //장소 좋아요 목룍 불러오기
-    PLACE_LIKE_READ_SUCCESS: '장소 좋아요 목록 불러오기 성공',
-    PLACE_LIKE_READ_FAIL: '장소 좋아요 목록 불러오기 실패',
-
-    //일정 추가 헤헷
-    PLAN_CREATE_SUCCESS: '일정 추가 성공',
-    PLAN_CREATE_FAIL: '일정 추가 실패'
+    PLACE_LIKE_DELETE_FAIL: '장소 좋아요 삭제 실패'
 }
