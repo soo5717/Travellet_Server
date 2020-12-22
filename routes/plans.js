f6c3894cb73383f6232c255b6d202d0f3818a088
Jiseondf <48876106+Jiseondf@users.noreply.github.com>
2020-12-22 20:22:38 +0900

    plan

--- routes/plans.js
+++ routes/plans.js
@@ -3,7 +3,10 @@ var router = express.Router();
 const { checkToken } = require('../middleware/auth');
 const planController = require('../controllers/planController');
 
+// [POST] 일정 추가
+router.post('/:travelid/plans', checkToken, planController.createPlan);
+
 // [GET] 일정 조회
-router.get('/', checkToken, planController.readPlan);
+router.get('/:travelid/plans', checkToken, planController.readPlan);
 
 module.exports = router;
--- services/planService.js
+++ services/planService.js
@@ -1,6 +1,23 @@
 const { Plan } = require('../models');
 
 module.exports = {
+    createPlan: async (date, time, place, memo, category, transport, x, y, travelId) => {
+        try {
+            await Plan.create({
+                date: date,
+                time: time,
+                place: place,
+                memo: memo,
+                category: category,
+                transport: transport,
+                x: x,
+                y: y,                
+                travel_id: travelId
+            });            
+        } catch (e) {
+            throw e;
+        }
+    },
 
     readPlan: async (travelId) => {
         try {
