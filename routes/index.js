eb37df2a8ef59eec60606d8adea39ebf3300bd81
Jiseondf <48876106+Jiseondf@users.noreply.github.com>
2020-12-22 20:25:31 +0900

    plan uri

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res) {
 
 // Router 추가
 router.use('/users', require('./users'));
-router.use('/travels', require('./travels'));
-router.use('/travels/:travelid/plans', require('./plans'));
+//router.use('/travels', require('./travels'));
+router.use('/travels', require('./plans'));
 router.use('/budgets', require('./budgets'));
 router.use('/expenses', require('./expenses'));
 router.use('/likes', require('./placeLikes'));
