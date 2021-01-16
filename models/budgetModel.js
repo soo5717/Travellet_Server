4ef7819648438ab55bab605b6d242b89c37c7afc
soo5717 <koo5717@naver.com>
2021-01-17 03:33:34 +0900

    change budget, expense model type (boolean => int)

--- models/budgetModel.js
+++ models/budgetModel.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         category: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.INTEGER,
             allowNull: false
         }
     }, {
