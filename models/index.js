e5f05ba21fb1e251570bdfc88ccb92dce5f811d2
soo5717 <koo5717@naver.com>
2021-01-30 23:04:28 +0900

    fix(model): test => development

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'test';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
